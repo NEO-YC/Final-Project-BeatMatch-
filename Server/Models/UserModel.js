a1e0b6754f86fde700a8d41c9e646fc891a8112d
NEO-YC <neoray0099@gmail.com>
2025-11-17 11:50:09 +0200

    Fix: Add all Server files properly (was showing as empty submodule)

---
+++ Server/Models/UserModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  birthday: {
+    type: Date,
+    required: true
+  },
+
+}, {
+  timestamps: true 
+});
+
+
+
+module.exports=mongoose.model('P-users', userSchema);
+
+
+
+//for Postman register test
+// {
+//   "firstname": "Daniel",
+//   "lastname": "Nisan",
+//   "email": "Daniel@example.com",
+//   "password": "12xx345678",
+//   "birthday": "1997-04-17"
+// }
